b850391e88728296077f79d57ed30135b985900c
jeanlucesokia <jlb@esokia-webagency.com>
2022-06-30 09:08:22 +0300

    config code use prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react'
-import { Button, TextField, Box } from '@mui/material'
-import './App.css';
+import { useEffect, useState } from "react";
+import { Button, TextField, Box } from "@mui/material";
+import "./App.css";
 
 function App() {
-  const [name, setName] = useState('')
-  const [data, setData] = useState([])
+  const [name, setName] = useState("");
+  const [data, setData] = useState([]);
   const change = () => {
-    setData([...data, name])
-  }
-  useEffect(()=> {
-    setData(data)
-  }, [data])
+    setData([...data, name]);
+  };
+  useEffect(() => {
+    setData(data);
+  }, [data]);
   const reset = () => {
-    setData([])
-    setName('')
-  }
+    setData([]);
+    setName("");
+  };
   return (
     <div className="App">
       <Box
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          "& > :not(style)": { m: 1, width: "25ch" },
         }}
         noValidate
         autoComplete="off"
@@ -30,31 +30,29 @@ function App() {
           label="Enter text"
           onChange={(text) => setName(text.target.value)}
           onKeyPress={(e) => {
-            if(e.key=='Enter'){
-              if(name.length >= 2) return change()
+            if (e.key == "Enter") {
+              if (name.length >= 2) return change();
               return false;
             }
           }}
-          size='small'
+          size="small"
           value={name}
         />
-        <Button 
+        <Button
           disabled={name.length < 3 ? true : false}
           onClick={() => change()}
-          variant="contained" 
+          variant="contained"
           color="success"
-          >change text</Button>
-        <Button 
-          variant="outlined" 
-          color="error"
-          onClick={() => reset()}
-          >
+        >
+          change text
+        </Button>
+        <Button variant="outlined" color="error" onClick={() => reset()}>
           initilize
         </Button>
       </Box>
-      {
-        data.length != 0 ? data.map(x=><h1 key={Math.random()*101}>name: {x}</h1>):''
-      }
+      {data.length != 0
+        ? data.map((x) => <h1 key={Math.random() * 101}>name: {x}</h1>)
+        : ""}
     </div>
   );
 }
